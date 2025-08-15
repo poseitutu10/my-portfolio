1dcfc073c9e609e76981ba0f325d3e7b4b7a13e0
poseitutu10 <phinehasoseitutu1@gmail.com>
2025-08-15 12:51:28 +0000

    feat: about section implemented

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,7 +13,7 @@ const About: React.FC = () => {
   return (
     <section>
       <div className="flex flex-col md:flex-row gap-10 my-16">
-        <div className="md:w-1/4 p-5">
+        <div className="md:w-1/4 px-5">
           <Image
             src={profile}
             alt="profile-pic"
